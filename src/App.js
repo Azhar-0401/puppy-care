da577ebc8decbac4fcd5e71a86622f2207ee6cf7
Azhar Ansari <azharansari@Azhars-MacBook-Air.local>
2023-10-20 14:35:17 +0530

    puppy-care-master

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Navigation2 from './components/Navigation2.js';
+import Header from './components/Header.js';
+import MContainer from './components/MContainer.js';
+import M2Container from './components/M2Container';
+import M3Container from './components/M3Container';
+import M4Container from './components/M4Container';
+import M5Container from './components/M5Container';
+import M6Container from './components/M6Container';
+import M7Container from './components/M7Container';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Navigation2/>
+      <Header/>
+      <MContainer/>
+      <M2Container/>
+      <M3Container/>
+      <M4Container/>
+      <M5Container/>
+      <M6Container/>
+      <M7Container/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
